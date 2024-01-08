b2013e6610e5f2ba6b67e8c310bed96e778f5342
Loryn Duncombe <lorynduncombe1@gmail.com>
2024-01-08 14:28:00 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using gRPC;
+using Grpc.Net.Client;
+
+using var grpcChannel = GrpcChannel.ForAddress("http://localhost:5289");
+var grpcClient = new ToDoIt.ToDoItClient(grpcChannel);
+
+var reply =  grpcClient.ListToDo
+    (new GetAllRequest() { });
+
+
+Console.WriteLine(reply.ToDo);
+
+Console.ReadKey();
